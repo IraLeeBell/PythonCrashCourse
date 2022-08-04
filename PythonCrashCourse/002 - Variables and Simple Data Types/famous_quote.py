79e2bb689081b0a517ad1f61ec60245d81f5e2ec
Ira Bell <IraLeeBell@gmail.com>
2022-08-04 12:36:04 -0500

    Working through book exercises

--- PythonCrashCourse/002 - Variables and Simple Data Types/famous_quote.py
+++ PythonCrashCourse/002 - Variables and Simple Data Types/famous_quote.py
@@ -2,6 +2,9 @@ famous_person = "Robert Heinlein"
 famous_quote = "Always listen to the experts. \n\tThey'll tell you what can't be done, and why. Then do it."
 print(f"\n\t{famous_person} once said, {famous_quote}")
 
-
-
+famous_person_first = "robert"
+famous_person_last = "heinlein"
+famous_person_full_name = f"{famous_person_first.title()} {famous_person_last.title()}"
+famous_quote_additional = "Never attempt to teach a pig to sing; it wastes your time and annoys the pig."
+print(f"\n\t{famous_person_full_name} once said, {famous_quote_additional}")
 
