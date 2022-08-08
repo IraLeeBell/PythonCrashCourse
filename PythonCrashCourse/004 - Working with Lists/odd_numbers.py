df803f3b7dc130e7edbb651a4af0c48fccdc0999
Ira Bell <IraLeeBell@gmail.com>
2022-08-07 19:37:15 -0500

    Working through book exercises

---
+++ PythonCrashCourse/004 - Working with Lists/odd_numbers.py
@@ -0,0 +1,4 @@
+odd_numbers = list(range(1,20,2))
+for odd_number in odd_numbers:
+	print(odd_number)
+print("These are all of the odd numbers between 1 and 20!")
---
+++ PythonCrashCourse/004 - Working with Lists/summing_a_million.py
@@ -0,0 +1,8 @@
+numbers = list(range(1,1_000_001))
+varmin = min(numbers)
+varmax = max(numbers)
+print(varmin)
+print(varmax)
+
+varsum = sum(numbers)
+print(varsum)
