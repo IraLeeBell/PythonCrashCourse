3271bc775f518b8a989161e6cdec9942bfe311ea
Ira Bell <IraLeeBell@gmail.com>
2022-08-07 20:38:57 -0500

    Working through book exercises

---
+++ PythonCrashCourse/004 - Working with Lists/buffet.py
@@ -0,0 +1,8 @@
+blairs_food = ('pizza slice', 'cheese quesadilla', 'double bacon sandwich without meat', 'everything bagel', 'fries')
+for food in blairs_food:
+	print(food)
+
+blairs_food = ('chicken taco', 'cheese quesadilla', 'double bacon sandwich without meat', 'everything bagel', 'smoothie')
+print("Time for a menu change!")
+for food in blairs_food:
+	print(food)
