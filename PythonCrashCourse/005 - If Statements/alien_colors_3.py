7fa86af68e8b671e3f2a774ca66983ba9fdd4654
Ira Bell <IraLeeBell@gmail.com>
2022-08-09 16:17:06 -0500

    Working through book exercises

---
+++ PythonCrashCourse/005 - If Statements/alien_colors_3.py
@@ -0,0 +1,61 @@
+alien_color = 'green'
+if alien_color == 'green':
+    points = 5
+    print(f"You just earned {points} points.")
+elif alien_color == 'yellow':
+    points = 10
+    print(f"You just earned {points} points.")
+elif alien_color == 'red':
+    points = 15
+    print(f"You just earned {points} points.")
+else:
+    points = 0
+    print(f"You did not earn any points on this round.")
+
+# Now to execute the first elif
+
+alien_color = 'yellow'
+if alien_color == 'green':
+    points = 5
+    print(f"You just earned {points} points.")
+elif alien_color == 'yellow':
+    points = 10
+    print(f"You just earned {points} points.")
+elif alien_color == 'red':
+    points = 15
+    print(f"You just earned {points} points.")
+else:
+    points = 0
+    print(f"You did not earn any points on this round.")
+
+# Now to execute the second elif
+
+alien_color = 'red'
+if alien_color == 'green':
+    points = 5
+    print(f"You just earned {points} points.")
+elif alien_color == 'yellow':
+    points = 10
+    print(f"You just earned {points} points.")
+elif alien_color == 'red':
+    points = 15
+    print(f"You just earned {points} points.")
+else:
+    points = 0
+    print(f"You did not earn any points on this round.")
+
+# Now to execute the else
+
+alien_color = 'blue'
+if alien_color == 'green':
+    points = 5
+    print(f"You just earned {points} points.")
+elif alien_color == 'yellow':
+    points = 10
+    print(f"You just earned {points} points.")
+elif alien_color == 'red':
+    points = 15
+    print(f"You just earned {points} points.")
+else:
+    points = 0
+    print(f"You did not earn any points on this round.")
