2af4f73a03cff52d78802e43bd5747c55fe89dac
Ira Bell <ira@irabell.com>
2022-08-11 17:34:04 -0500

    Working through book exercises

---
+++ PythonCrashCourse/005 - If Statements/ordinal_numbers.py
@@ -0,0 +1,23 @@
+numbers = [1,2,3,4,5,6,7,8,9]
+
+for number in numbers:
+	if number == 1:
+		print("\n1st")
+	elif number == 2:
+		print("\n2nd")
+	elif number == 3:
+		print("\n3rd")
+	elif number == 4:
+		print("\n4th")
+	elif number == 5:
+		print("\n5th")
+	elif number == 6:
+		print("\n6th")
+	elif number == 7:
+		print("\n7th")
+	elif number == 8:
+		print("\n8th")
+	elif number == 9:
+		print("\n9th")
+	else:
+		print("We didn't find an ordinal number matching 1-9 to output.")
