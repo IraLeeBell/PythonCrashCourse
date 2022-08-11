2d7edc103beb565e38e603c1153063632c47e41c
Ira Bell <ira@irabell.com>
2022-08-11 18:35:40 -0500

    Working through book exercises...

---
+++ PythonCrashCourse/006 - Dictionaries/glossary.py
@@ -0,0 +1,16 @@
+glossary = {
+	'Python': 'A programming language that is highly readable and functional',
+	'List': 'A way to store several object in python which you can iterate through and read, etc.',
+	'Integer': 'A number, such as 1 or 9.',
+	'Boolean Value': 'True or False',
+	'Is Equal To?': 'The operator ==, when used, can evaluate whether two things are equal to each other.'
+
+	}
+
+print("The following are some things that I've learned about Python:")
+print("\n")
+print("\nPython:\n" + glossary['Python'])
+print("\nList:\n" + glossary['List'])
+print("\nInteger:\n" + glossary['Integer'])
+print("\nBoolean Value:\n" + glossary['Boolean Value'])
+print("\nIs Equal To?:\n" + glossary['Is Equal To?'])
