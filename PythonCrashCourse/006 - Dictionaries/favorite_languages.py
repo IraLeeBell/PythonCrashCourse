457e7711884fc0b5030a5e86c47981476f9c11e0
Ira Bell <ira@irabell.com>
2022-08-11 18:58:45 -0500

    Working through book exercises

--- PythonCrashCourse/006 - Dictionaries/favorite_languages.py
+++ PythonCrashCourse/006 - Dictionaries/favorite_languages.py
@@ -6,4 +6,10 @@ favorite_languages = {
 }
 
 language = favorite_languages['sara'].upper()
-print(f"Sara's favorite lanugae is {language}.")
+print(f"Sara's favorite language is {language}.")
+
+
+# Now let's loop through the list
+
+for name, language in favorite_languages.items():
+	print(f"{name.title()}'s favorite language is {language.title()}")
