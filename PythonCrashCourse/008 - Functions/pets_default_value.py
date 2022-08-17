1d0194a1d13aabfa787fba5f9bd8cd87a513f207
Ira Bell <ira@irabell.com>
2022-08-17 12:10:03 -0500

    Working through book exercises

--- PythonCrashCourse/008 - Functions/pets_default_value.py
+++ PythonCrashCourse/008 - Functions/pets_default_value.py
@@ -9,3 +9,17 @@ describe_pet(pet_name = 'dottie')
 # Note that the below also works, but probably isn't a best practice
 
 describe_pet('dottie')
+
+# You can replace the animal type if desired, even though it has
+# a default value:
+
+describe_pet(pet_name = 'max', animal_type = 'cat')
+
+# A dog named Willie.
+describe_pet('willie')
+describe_pet(pet_name = 'willie')
+
+# A cat named Max.
+describe_pet('max', 'cat')
+describe_pet(pet_name = 'max', animal_type = 'cat')
+describe_pet(animal_type = 'cat', pet_name = 'max')
