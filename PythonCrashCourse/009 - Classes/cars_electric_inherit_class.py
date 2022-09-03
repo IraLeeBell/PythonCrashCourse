f3b3693451ae18f07c04827a3bcf87ad85ce2cec
Ira Bell <ira@irabell.com>
2022-09-03 18:42:10 -0500

    Working through book exercises...

--- PythonCrashCourse/009 - Classes/cars_electric_inherit_class.py
+++ PythonCrashCourse/009 - Classes/cars_electric_inherit_class.py
@@ -28,6 +28,23 @@ class Car:
 	def fill_gas_tank(self):
 		print(f"You have just filled up the gas tank in your {self.year} {self.make} {self.model}!")
 
+class Battery:
+	def __init__(self, battery_size=75):
+		"""Initialize the battery's attributes"""
+		self.battery_size = battery_size
+
+	def describe_battery(self):
+		print(f"This car has a {self.battery_size} kWh battery.")
+
+	def get_range(self):
+		"""Print a statement about a range this battery provides."""
+		if self.battery_size == 75:
+			range = 260
+		elif self.battery_size == 100:
+			range = 315
+
+		print(f"This car can go about {range} miles on a full charge.")
+
 class ElectricCar(Car):
 	"""Represent aspects of cars, specific to electric vehicles"""
 	def __init__(self, make, model, year):
@@ -36,12 +53,7 @@ class ElectricCar(Car):
 		Then initialize attribute specific to the electric car.
 		"""
 		super().__init__(make, model, year)
-		self.battery_size = 75
-
-	
-	def describe_battery(self):
-		"""Print a statement describing the battery size."""
-		print(f"This car has a {self.battery_size} kWh battery.")
+		self.battery = Battery()
 
 	def fill_gas_tank(self):
 		print(f"Electric cars do not need gas, so this isn't necessary!")
@@ -55,5 +67,6 @@ my_tesla = ElectricCar('tesla', 'plaid', '2022')
 print(my_tesla.get_descriptive_name())
 my_tesla.increment_odometer(42)
 my_tesla.read_odometer()
-my_tesla.describe_battery()
+my_tesla.battery.describe_battery()
 my_tesla.fill_gas_tank()
+my_tesla.battery.get_range()
