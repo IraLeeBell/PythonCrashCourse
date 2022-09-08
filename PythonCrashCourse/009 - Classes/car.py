186830a04944fdca18766782280cdefe1d7856bd
Ira Bell <ira@irabell.com>
2022-09-08 13:52:16 -0500

    Working through book exercises...

--- PythonCrashCourse/009 - Classes/car.py
+++ PythonCrashCourse/009 - Classes/car.py
@@ -27,3 +27,32 @@ class Car:
 
 	def fill_gas_tank(self):
 		print(f"You have just filled up the gas tank in your {self.year} {self.make} {self.model}!")
+
+class Battery:
+	def __init__(self, battery_size=75):
+		"""Initialize the battery's attributes"""
+		self.battery_size = battery_size
+
+	def describe_battery(self):
+		print(f"This car has a {self.battery_size} kWh battery.")
+
+	def get_range(self):
+		"""Print a statement about a range this battery provides."""
+		if self.battery_size == 75:
+			range = 260
+		elif self.battery_size == 100:
+			range = 315
+
+		print(f"This car can go about {range} miles on a full charge.")
+
+class ElectricCar(Car):
+	"""Models aspects of a car, specific to electric vehicles."""
+
+	def __init__(self, make, model, year):
+		"""
+		Initialize attributes of the parent class.
+		Then initialize attributes specific to an electric car.
+		"""
+		super().__init__(make, model, year)
+		self.battery = Battery()
+
