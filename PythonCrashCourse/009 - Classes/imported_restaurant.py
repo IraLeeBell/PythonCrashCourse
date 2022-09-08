4967392b93c5129db2615e54714a9ff2172598e4
Ira Bell <ira@irabell.com>
2022-09-08 14:17:56 -0500

    Working through book exercises...

---
+++ PythonCrashCourse/009 - Classes/imported_restaurant.py
@@ -0,0 +1,6 @@
+from restaurant_module import Restaurant
+
+my_business = Restaurant('The Pizza Place', 'Italian')
+
+my_business.open_restaurant()
+my_business.close_restaurant()
--- PythonCrashCourse/009 - Classes/my_cars.py
+++ PythonCrashCourse/009 - Classes/my_cars.py
@@ -2,12 +2,20 @@
 # from car import Car, ElectricCar
 
 # When we use the below, we have to use dot notation to call the classes (i.t. car.ElectricCar)
-import car
+# import car
 
-my_beetle = car.Car('volkswagon', 'beetle', 2019)
+# When we use the below we don't need to use dot notation
+# This approach is not recommended since it's unclear which classes you're
+# using from a given module, but it does work. 
+# from car import *
+
+# We can also use aliases, like shown below
+from car import Car as C, ElectricCar as EC
+
+my_beetle = C('volkswagon', 'beetle', 2019)
 print(my_beetle.get_descriptive_name())
 
-my_tesla = car.ElectricCar('tesla', 'model 3 performance edition', 2022)
+my_tesla = EC('tesla', 'model 3 performance edition', 2022)
 print(my_tesla.get_descriptive_name())
 my_tesla.battery.describe_battery()
 my_tesla.battery.get_range()
