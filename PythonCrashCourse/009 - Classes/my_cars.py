89e6eb2dfcb7d7d7d066bbc46302a6c34dd70ce6
Ira Bell <ira@irabell.com>
2022-09-08 14:06:06 -0500

    Working through book exercises...

--- PythonCrashCourse/009 - Classes/my_cars.py
+++ PythonCrashCourse/009 - Classes/my_cars.py
@@ -1,9 +1,13 @@
-from car import Car, ElectricCar
+# When we use the below we don't need to use dot notation
+# from car import Car, ElectricCar
 
-my_beetle = Car('volkswagon', 'beetle', 2019)
+# When we use the below, we have to use dot notation to call the classes (i.t. car.ElectricCar)
+import car
+
+my_beetle = car.Car('volkswagon', 'beetle', 2019)
 print(my_beetle.get_descriptive_name())
 
-my_tesla = ElectricCar('tesla', 'model 3 performance edition', 2022)
+my_tesla = car.ElectricCar('tesla', 'model 3 performance edition', 2022)
 print(my_tesla.get_descriptive_name())
 my_tesla.battery.describe_battery()
 my_tesla.battery.get_range()
