e173d71700432dc8a7a26f6531e869d24e809a0d
Ira Bell <ira@irabell.com>
2022-09-08 14:26:37 -0500

    Working through book exercises...

---
+++ PythonCrashCourse/009 - Classes/admin_module.py
@@ -0,0 +1,10 @@
+class Privileges:
+	def get_privileges3(self):
+		privileges3=['can add post', 'can delete post', 'can ban user']
+		return privileges3
+
+class Administrators(Users):
+	def __init__(self, first_name, last_name, age, birth_city, allergies):
+		super().__init__(first_name, last_name, age, birth_city, allergies)
+
+		self.show_priv = Privileges()
